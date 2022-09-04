6014294f0afc6c8e21e78a9068c46d750e490aab
wighawag <wighawag@gmail.com>
2022-09-04 12:50:43 +0100

    dev period 0

--- deploy/001_deploy_bleeps_demo.ts
+++ deploy/001_deploy_bleeps_demo.ts
@@ -4,12 +4,14 @@ import {DeployFunction} from 'hardhat-deploy/types';
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 	const {deployments, getNamedAccounts} = hre;
 	const {deploy} = deployments;
+	const useProxy = !hre.network.live;
 
 	const {deployer} = await getNamedAccounts();
 
 	await deploy('BleepsDemo', {
 		from: deployer,
 		args: [],
+		proxy: useProxy,
 		log: true,
 		autoMine: true // speed up deployment on local network (ganache, hardhat), no effect on live networks
 	});
