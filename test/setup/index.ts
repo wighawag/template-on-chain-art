cbff5689fe9f15bb633a03b18bb1a962a85d2de6
wighawag <wighawag@gmail.com>
2022-09-07 12:22:40 +0100

    fix

--- test/setup/index.ts
+++ test/setup/index.ts
@@ -1,14 +1,11 @@
 import {ethers, deployments, getUnnamedAccounts} from 'hardhat';
 import {setupUsers} from '../utils/users';
-import {setTimestamp} from '../utils/time';
-import {nextSunday} from '../../utils/time';
 import {MandalaToken} from '../../typechain';
 
 export const CommitmentHashZero = '0x000000000000000000000000000000000000000000000000';
 
 export const setup = deployments.createFixture(async () => {
 	await deployments.fixture('MandalaToken');
-	await setTimestamp(nextSunday());
 	const contracts = {
 		MandalaToken: <MandalaToken>await ethers.getContract('MandalaToken')
 	};
